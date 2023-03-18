7c5a1f8b2cb0563eefd39b5d4432ff3703c7adfa
Amirreza Heydari | امیررضا حیدری <amirreza09395574353@gmail.com>
2023-03-18 19:46:12 +0330

    new update

---
+++ calender-dark/js/calendar.js
@@ -0,0 +1,11 @@
+let options = {
+
+    placeholder: "تاریخ موردنظر را انتخاب کنید",
+    twodigit: false,
+    markToday: true,
+    highlightSelectedDay: true,
+    gotoToday: true,
+    pastYearsCount: 90,
+}
+kamaDatepicker('demo', options);
+// https://www.jqueryscript.net/time-clock/Persian-Jalali-Calendar-Data-Picker-Plugin-With-jQuery-kamaDatepicker.html
---
+++ calender-project/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'danapro': ['danapro'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
